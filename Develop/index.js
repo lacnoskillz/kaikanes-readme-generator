69b5d9cf6c86a0a7d2434daa7c3cb677a28fdc39
lacnoskillz <kaikane3457@hotmail.com>
2022-12-08 14:12:21 -0600

    added the other questions usage/licensing/contibution/tests/questiongs/installation. sucessfully added user input data to create readme. added github link. email.

--- Develop/index.js
+++ Develop/index.js
@@ -6,9 +6,27 @@ const questions = [];
 
 // TODO: Create a function to write README file
 function writeToFile(filename, data) { 
-  
-  fs.writeFile(filename, '# ' + data.name + '\n ![](https://img.shields.io/badge/licence-'+data.license+'-brightgreen)'+'\n' + '## Description' + '\n\n' + data.descript + '\n\n## Table of Contents \n\n-[Installation](#installation) \n-[Usage](#usage)\n-[Contributing](#contributing)\n-[Tests](#tests)', (err) =>
-    err ? console.log(err) : console.log('success!')
+  let http = "https://github.com/"+data.github+"?tab=repositories";
+  fs.writeFile(filename,
+     '# ' + data.name 
+     + '\n ![](https://img.shields.io/badge/licence-'+data.license+'-brightgreen)'
+     +'\n' + '## Description' + '\n\n' + data.descript 
+     + '\n\n## Table of Contents \n\n* [Installation](#installation) \n* [Usage](#usage)\n* [License](#license)\n* [Contributing](#contributing)\n* [Tests](#tests)\n* [Questions](#questions)'
+     + '\n\n## Installation'
+     +'\nTo install necessary dependencies run the following command:'
+     +'\n\n           '+data.install
+     +'\n\n## Usage'
+     +'\n'+data.use
+     +'\n\n## License'
+     +'\nThis project is licensed under the '+data.license+' license'
+     +'\n\n## Contributing'
+     +'\n'+data.contrib
+     +'\n\n## Tests'
+     +'\nTo run tests run the following command:'
+     +'\n\n           '+data.test
+     +'\n\n## Questions'
+     +'\nIf you have any questions about the repo open an issue or contect me directly at '+'['+data.email+']'+'(https://google.com/)'+'. You can find more of my work at '+'['+data.github+']'+'('+http+')'+'.', 
+     (err) => err ? console.log(err) : console.log('README generating...')
   );
 
 }
@@ -42,7 +60,28 @@ inquirer
       name: 'license',
       choices: ['MIT', 'APACHE2.0', 'GPL3.0', 'BSD3', 'None']
     },
-
+    {
+      type: 'input',
+      message: 'what command should be run to install dependencies?',
+      default: "npm i",
+      name: 'install'
+    },
+    {
+      type: 'input',
+      message: 'what command should be run to run tests?',
+      default: "npm test",
+      name: 'test'
+    },
+    {
+      type: 'input',
+      message: 'what does the user need to know about using the repo?',
+      name: 'use'
+    },
+    {
+      type: 'input',
+      message: 'what does the user need to know about contributions?',
+      name: 'contrib'
+    },
   ])
 
   .then((data) => {
