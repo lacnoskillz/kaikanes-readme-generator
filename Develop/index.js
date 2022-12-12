51d959b6812671c834e4bdb87d0c185d7726ee00
lacnoskillz <kaikane3457@hotmail.com>
2022-12-12 13:58:41 -0600

    used generateMarkdown.js folder for readme template instead. used functions in there for badges aswell. moved  inquirer inside init()

--- Develop/index.js
+++ Develop/index.js
@@ -1,93 +1,79 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    message: 'What is yout github username?',
+    name: 'github'
+  },
+  {
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'email'
+  },
+  {
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'name'
+  },
+  {
+    type: 'input',
+    message: 'Give a description of your project',
+    name: 'descript'
+  },
+  {
+    type: 'list',
+    message: 'What license should your project have?',
+    name: 'license',
+    choices: ['MIT', 'APACHE2.0', 'GPL3.0', 'BSD3', 'None']
+  },
+  {
+    type: 'input',
+    message: 'what command should be run to install dependencies?',
+    default: "npm i",
+    name: 'install'
+  },
+  {
+    type: 'input',
+    message: 'what command should be run to run tests?',
+    default: "npm test",
+    name: 'test'
+  },
+  {
+    type: 'input',
+    message: 'what does the user need to know about using the repo?',
+    name: 'use'
+  },
+  {
+    type: 'input',
+    message: 'what does the user need to know about contributions?',
+    name: 'contrib'
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(filename, data) { 
-  let http = "https://github.com/"+data.github+"?tab=repositories";
-  fs.writeFile(filename,
-     '# ' + data.name 
-     + '\n ![](https://img.shields.io/badge/licence-'+data.license+'-brightgreen)'
-     +'\n' + '## Description' + '\n\n' + data.descript 
-     + '\n\n## Table of Contents \n\n* [Installation](#installation) \n* [Usage](#usage)\n* [License](#license)\n* [Contributing](#contributing)\n* [Tests](#tests)\n* [Questions](#questions)'
-     + '\n\n## Installation'
-     +'\nTo install necessary dependencies run the following command:'
-     +'\n\n           '+data.install
-     +'\n\n## Usage'
-     +'\n'+data.use
-     +'\n\n## License'
-     +'\nThis project is licensed under the '+data.license+' license'
-     +'\n\n## Contributing'
-     +'\n'+data.contrib
-     +'\n\n## Tests'
-     +'\nTo run tests run the following command:'
-     +'\n\n           '+data.test
-     +'\n\n## Questions'
-     +'\nIf you have any questions about the repo open an issue or contect me directly at '+'['+data.email+']'+'(https://google.com/)'+'. You can find more of my work at '+'['+data.github+']'+'('+http+')'+'.', 
-     (err) => err ? console.log(err) : console.log('README generating...')
+function writeToFile(filename, data) {
+  const markdown = generateMarkdown(data);
+  // const http = "https://github.com/"+data.github+"?tab=repositories";
+  fs.writeFile(filename, markdown,
+
+    (err) => err ? console.log(err) : console.log('README generating...')
   );
 
 }
 // TODO: Create a function to initialize app
-function init() { }
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: 'What is yout github username?',
-      name: 'github'
-    },
-    {
-      type: 'input',
-      message: 'What is your email address?',
-      name: 'email'
-    },
-    {
-      type: 'input',
-      message: 'What is the title of your project?',
-      name: 'name'
-    },
-    {
-      type: 'input',
-      message: 'Give a description of your project',
-      name: 'descript'
-    },
-    {
-      type: 'list',
-      message: 'What license should your project have?',
-      name: 'license',
-      choices: ['MIT', 'APACHE2.0', 'GPL3.0', 'BSD3', 'None']
-    },
-    {
-      type: 'input',
-      message: 'what command should be run to install dependencies?',
-      default: "npm i",
-      name: 'install'
-    },
-    {
-      type: 'input',
-      message: 'what command should be run to run tests?',
-      default: "npm test",
-      name: 'test'
-    },
-    {
-      type: 'input',
-      message: 'what does the user need to know about using the repo?',
-      name: 'use'
-    },
-    {
-      type: 'input',
-      message: 'what does the user need to know about contributions?',
-      name: 'contrib'
-    },
-  ])
+function init() {
+  inquirer
+    .prompt(questions)
+  
+   .then((data) => {
+     const filename = `README.md`;
+     writeToFile(filename, data);
+   });
+}
 
-  .then((data) => {
-    const filename = `README.md`;
-    writeToFile(filename, data);
-  });
-
-// Function call to initialize app
-init();
+  // Function call to initialize app
+  init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,60 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+        if(license == "None"){
+                return "";
+        }else{
+        return `![licensebadge](https://img.shields.io/badge/licence-${license}-brightgreen)`
+        
+        }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+        if(license == "None"){
+                return "This project is not under license"
+        }else{
+                return `this project is licensed under ${license} license`
+        }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const licenceBadge = renderLicenseBadge(data.license);
+  const licenseSection = renderLicenseSection(data.license)
+        return `
+  # ${data.name} 
+  ${licenceBadge}
+  ## Description
+  ${data.descript} 
+  
+  ## Table of Contents 
+  * [Installation](#installation) 
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+  To install necessary dependencies run the following command:
+  
+          ${data.install}
 
+  ## Usage
+  ${data.use}
+  
+  ## License
+  ${licenseSection}
+  
+  ## Contributing
+  ${data.contrib}
+  ## Tests
+  To run tests run the following command:
+          ${data.test}
+  ## Questions'
+  If you have any questions about the repo open an issue or contect me directly at [${data.email}](https://google.com/). You can find more of my work at [${data.github}]('+http+').
 `;
 }
 
